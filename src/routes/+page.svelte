d2e2d0f5fd8b116b649c2693748390453a814de9
Y-Less <github@y-less.com>
2025-02-06 17:48:08 +0100

    Better colours, using HSL.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,32 +23,30 @@
 	
 	const
 		GRID = {
-			A: ['D0EDD4', '14FAEB', '886C92', 'E72866', '002020', '71AFD7', '7476F0'],
-			B: ['CAEBCF', '1EF8E1', '81728C', 'E8266E', '002626', '77B2D9', '6D7CEF'],
-			C: ['C5E9CA', '28F6D7', '7B7886', 'E92377', '002B2B', '7CB5DA', '6782EF'],
-			D: ['C0E7C5', '33F3CD', '747E80', 'EB217F', '003030', '82B8DC', '6087EE'],
-			E: ['BBE5C1', '3DF1C3', '6D847A', 'EC1F88', '003535', '87BBDD', '5A8DED'],
-			F: ['B6E3BC', '47EFB9', '678A74', 'ED1D90', '003B3B', '8DBEDF', '5393ED'],
-			G: ['B0E1B7', '51EDAF', '60906E', 'EF1A99', '004040', '92C1E0', '4C98EC'],
-			H: ['ABDFB2', '5BEAA5', '5A9668', 'F018A1', '004545', '97C4E2', '469EEB'],
-			I: ['A6DDAE', '66E89B', '539C62', 'F116AA', '004B4B', '9DC7E3', '3FA4EB'],
-			J: ['A1DBA9', '70E691', '4CA25C', 'F314B2', '005050', 'A2CAE5', '39A9EA'],
-			K: ['9CDAA4', '7AE487', '46A856', 'F411BB', '005555', 'A8CDE6', '32AFE9'],
-			L: ['96D89F', '84E17D', '3FAE50', 'F50FC3', '005A5A', 'ADD1E8', '2CB5E9'],
-			M: ['91D69A', '8EDF73', '39B54A', 'F70DCC', '006060', 'B3D4E9', '2CB5E1'],
-			N: ['8CD496', '99DD69', '42B846', 'F80BD4', '006565', 'B8D7EB', '2DB5D9'],
-			O: ['87D291', 'A3DB5F', '4BBB43', 'F908DD', '006A6A', 'BDDAEC', '2DB5D2'],
-			P: ['81D08C', 'ADD855', '54BF3F', 'FB06E5', '007070', 'C3DDEE', '2EB5CA'],
-			Q: ['7CCE87', 'B7D64B', '5DC23C', 'FC04EE', '007575', 'C8E0EF', '2FB5C3'],
-			R: ['77CC83', 'C1D441', '66C539', 'FD02F6', '007A7A', 'CEE3F1', '2FB5BB'],
-			S: ['72CA7E', 'CCD237', '6FC935', 'FF00FF', '008080', 'D3E6F2', '30B5B4'],
-			T: ['6DC879', 'D6CF2D', '78CC32', 'F300FF', '008585', 'D9E9F4', '30B5AC'],
-			U: ['67C674', 'E0CD23', '81CF2F', 'E700FF', '008A8A', 'DEECF5', '31B5A4'],
-			V: ['62C470', 'EACB19', '8AD32B', 'DC00FF', '008F8F', 'E3EFF7', '32B59D'],
-			W: ['5DC26B', 'F4C90F', '93D628', 'D000FF', '009595', 'E9F2F8', '32B595'],
-			X: ['58C066', 'FFC706', '9CDA25', 'C500FF', '009A9A', 'EEF5FA', '33B58E'],
-			Y: ['53BE61', 'FEC709', 'A5DD21', 'B900FF', '009F9F', 'F4F8FB', '34B586'],
-			Z: ['4DBC5D', 'FDC70D', 'AEE01E', 'AD00FF', '00A5A5', 'F9FBFD', '34B57F'],
+			A: [ 'hsl(  0deg   25%   30%)', 'hsl(  0deg   50%   30%)', 'hsl(  0deg   75%   30%)', 'hsl(  0deg  100%   30%)', 'hsl(  0deg  100%   40%)', 'hsl(  0deg  100%   50%)', 'hsl(  0deg  100%   60%)', 'hsl(  0deg  100%   70%)', 'hsl(  0deg  100%   80%)', 'hsl(  0deg  100%   90%)' ],
+			B: [ 'hsl( 15deg   25%   30%)', 'hsl( 15deg   50%   30%)', 'hsl( 15deg   75%   30%)', 'hsl( 15deg  100%   30%)', 'hsl( 15deg  100%   40%)', 'hsl( 15deg  100%   50%)', 'hsl( 15deg  100%   60%)', 'hsl( 15deg  100%   70%)', 'hsl( 15deg  100%   80%)', 'hsl( 15deg  100%   90%)' ],
+			C: [ 'hsl( 30deg   25%   30%)', 'hsl( 30deg   50%   30%)', 'hsl( 30deg   75%   30%)', 'hsl( 30deg  100%   30%)', 'hsl( 30deg  100%   40%)', 'hsl( 30deg  100%   50%)', 'hsl( 30deg  100%   60%)', 'hsl( 30deg  100%   70%)', 'hsl( 30deg  100%   80%)', 'hsl( 30deg  100%   90%)' ],
+			D: [ 'hsl( 45deg   25%   30%)', 'hsl( 45deg   50%   30%)', 'hsl( 45deg   75%   30%)', 'hsl( 45deg  100%   30%)', 'hsl( 45deg  100%   40%)', 'hsl( 45deg  100%   50%)', 'hsl( 45deg  100%   60%)', 'hsl( 45deg  100%   70%)', 'hsl( 45deg  100%   80%)', 'hsl( 45deg  100%   90%)' ],
+			E: [ 'hsl( 60deg   25%   30%)', 'hsl( 60deg   50%   30%)', 'hsl( 60deg   75%   30%)', 'hsl( 60deg  100%   30%)', 'hsl( 60deg  100%   40%)', 'hsl( 60deg  100%   50%)', 'hsl( 60deg  100%   60%)', 'hsl( 60deg  100%   70%)', 'hsl( 60deg  100%   80%)', 'hsl( 60deg  100%   90%)' ],
+			F: [ 'hsl( 75deg   25%   30%)', 'hsl( 75deg   50%   30%)', 'hsl( 75deg   75%   30%)', 'hsl( 75deg  100%   30%)', 'hsl( 75deg  100%   40%)', 'hsl( 75deg  100%   50%)', 'hsl( 75deg  100%   60%)', 'hsl( 75deg  100%   70%)', 'hsl( 75deg  100%   80%)', 'hsl( 75deg  100%   90%)' ],
+			G: [ 'hsl( 90deg   25%   30%)', 'hsl( 90deg   50%   30%)', 'hsl( 90deg   75%   30%)', 'hsl( 90deg  100%   30%)', 'hsl( 90deg  100%   40%)', 'hsl( 90deg  100%   50%)', 'hsl( 90deg  100%   60%)', 'hsl( 90deg  100%   70%)', 'hsl( 90deg  100%   80%)', 'hsl( 90deg  100%   90%)' ],
+			H: [ 'hsl(105deg   25%   30%)', 'hsl(105deg   50%   30%)', 'hsl(105deg   75%   30%)', 'hsl(105deg  100%   30%)', 'hsl(105deg  100%   40%)', 'hsl(105deg  100%   50%)', 'hsl(105deg  100%   60%)', 'hsl(105deg  100%   70%)', 'hsl(105deg  100%   80%)', 'hsl(105deg  100%   90%)' ],
+			I: [ 'hsl(120deg   25%   30%)', 'hsl(120deg   50%   30%)', 'hsl(120deg   75%   30%)', 'hsl(120deg  100%   30%)', 'hsl(120deg  100%   40%)', 'hsl(120deg  100%   50%)', 'hsl(120deg  100%   60%)', 'hsl(120deg  100%   70%)', 'hsl(120deg  100%   80%)', 'hsl(120deg  100%   90%)' ],
+			J: [ 'hsl(135deg   25%   30%)', 'hsl(135deg   50%   30%)', 'hsl(135deg   75%   30%)', 'hsl(135deg  100%   30%)', 'hsl(135deg  100%   40%)', 'hsl(135deg  100%   50%)', 'hsl(135deg  100%   60%)', 'hsl(135deg  100%   70%)', 'hsl(135deg  100%   80%)', 'hsl(135deg  100%   90%)' ],
+			K: [ 'hsl(150deg   25%   30%)', 'hsl(150deg   50%   30%)', 'hsl(150deg   75%   30%)', 'hsl(150deg  100%   30%)', 'hsl(150deg  100%   40%)', 'hsl(150deg  100%   50%)', 'hsl(150deg  100%   60%)', 'hsl(150deg  100%   70%)', 'hsl(150deg  100%   80%)', 'hsl(150deg  100%   90%)' ],
+			L: [ 'hsl(165deg   25%   30%)', 'hsl(165deg   50%   30%)', 'hsl(165deg   75%   30%)', 'hsl(165deg  100%   30%)', 'hsl(165deg  100%   40%)', 'hsl(165deg  100%   50%)', 'hsl(165deg  100%   60%)', 'hsl(165deg  100%   70%)', 'hsl(165deg  100%   80%)', 'hsl(165deg  100%   90%)' ],
+			M: [ 'hsl(180deg   25%   30%)', 'hsl(180deg   50%   30%)', 'hsl(180deg   75%   30%)', 'hsl(180deg  100%   30%)', 'hsl(180deg  100%   40%)', 'hsl(180deg  100%   50%)', 'hsl(180deg  100%   60%)', 'hsl(180deg  100%   70%)', 'hsl(180deg  100%   80%)', 'hsl(180deg  100%   90%)' ],
+			N: [ 'hsl(195deg   25%   30%)', 'hsl(195deg   50%   30%)', 'hsl(195deg   75%   30%)', 'hsl(195deg  100%   30%)', 'hsl(195deg  100%   40%)', 'hsl(195deg  100%   50%)', 'hsl(195deg  100%   60%)', 'hsl(195deg  100%   70%)', 'hsl(195deg  100%   80%)', 'hsl(195deg  100%   90%)' ],
+			O: [ 'hsl(210deg   25%   30%)', 'hsl(210deg   50%   30%)', 'hsl(210deg   75%   30%)', 'hsl(210deg  100%   30%)', 'hsl(210deg  100%   40%)', 'hsl(210deg  100%   50%)', 'hsl(210deg  100%   60%)', 'hsl(210deg  100%   70%)', 'hsl(210deg  100%   80%)', 'hsl(210deg  100%   90%)' ],
+			P: [ 'hsl(225deg   25%   30%)', 'hsl(225deg   50%   30%)', 'hsl(225deg   75%   30%)', 'hsl(225deg  100%   30%)', 'hsl(225deg  100%   40%)', 'hsl(225deg  100%   50%)', 'hsl(225deg  100%   60%)', 'hsl(225deg  100%   70%)', 'hsl(225deg  100%   80%)', 'hsl(225deg  100%   90%)' ],
+			Q: [ 'hsl(240deg   25%   30%)', 'hsl(240deg   50%   30%)', 'hsl(240deg   75%   30%)', 'hsl(240deg  100%   30%)', 'hsl(240deg  100%   40%)', 'hsl(240deg  100%   50%)', 'hsl(240deg  100%   60%)', 'hsl(240deg  100%   70%)', 'hsl(240deg  100%   80%)', 'hsl(240deg  100%   90%)' ],
+			R: [ 'hsl(255deg   25%   30%)', 'hsl(255deg   50%   30%)', 'hsl(255deg   75%   30%)', 'hsl(255deg  100%   30%)', 'hsl(255deg  100%   40%)', 'hsl(255deg  100%   50%)', 'hsl(255deg  100%   60%)', 'hsl(255deg  100%   70%)', 'hsl(255deg  100%   80%)', 'hsl(255deg  100%   90%)' ],
+			S: [ 'hsl(270deg   25%   30%)', 'hsl(270deg   50%   30%)', 'hsl(270deg   75%   30%)', 'hsl(270deg  100%   30%)', 'hsl(270deg  100%   40%)', 'hsl(270deg  100%   50%)', 'hsl(270deg  100%   60%)', 'hsl(270deg  100%   70%)', 'hsl(270deg  100%   80%)', 'hsl(270deg  100%   90%)' ],
+			T: [ 'hsl(285deg   25%   30%)', 'hsl(285deg   50%   30%)', 'hsl(285deg   75%   30%)', 'hsl(285deg  100%   30%)', 'hsl(285deg  100%   40%)', 'hsl(285deg  100%   50%)', 'hsl(285deg  100%   60%)', 'hsl(285deg  100%   70%)', 'hsl(285deg  100%   80%)', 'hsl(285deg  100%   90%)' ],
+			U: [ 'hsl(300deg   25%   30%)', 'hsl(300deg   50%   30%)', 'hsl(300deg   75%   30%)', 'hsl(300deg  100%   30%)', 'hsl(300deg  100%   40%)', 'hsl(300deg  100%   50%)', 'hsl(300deg  100%   60%)', 'hsl(300deg  100%   70%)', 'hsl(300deg  100%   80%)', 'hsl(300deg  100%   90%)' ],
+			V: [ 'hsl(315deg   25%   30%)', 'hsl(315deg   50%   30%)', 'hsl(315deg   75%   30%)', 'hsl(315deg  100%   30%)', 'hsl(315deg  100%   40%)', 'hsl(315deg  100%   50%)', 'hsl(315deg  100%   60%)', 'hsl(315deg  100%   70%)', 'hsl(315deg  100%   80%)', 'hsl(315deg  100%   90%)' ],
+			W: [ 'hsl(330deg   25%   30%)', 'hsl(330deg   50%   30%)', 'hsl(330deg   75%   30%)', 'hsl(330deg  100%   30%)', 'hsl(330deg  100%   40%)', 'hsl(330deg  100%   50%)', 'hsl(330deg  100%   60%)', 'hsl(330deg  100%   70%)', 'hsl(330deg  100%   80%)', 'hsl(330deg  100%   90%)' ],
+			X: [ 'hsl(345deg   25%   30%)', 'hsl(345deg   50%   30%)', 'hsl(345deg   75%   30%)', 'hsl(345deg  100%   30%)', 'hsl(345deg  100%   40%)', 'hsl(345deg  100%   50%)', 'hsl(345deg  100%   60%)', 'hsl(345deg  100%   70%)', 'hsl(345deg  100%   80%)', 'hsl(345deg  100%   90%)' ],
 		},
 		COLS = Object.keys(GRID),
 		ROWS = Object.keys(GRID.A);
@@ -59,7 +57,7 @@
 		{
 			return '#FFFFFF';
 		}
-		return '#' + GRID[str[0]][str[1]];
+		return GRID[str[0]][str[1]];
 	}
 
 	function get(url)
@@ -253,7 +251,7 @@
 			<tr>
 				<th>{i}</th>
 	{#each COLS as j}
-				<td style="background-color: #{GRID[j][i]}" onclick={guess(i, j)}>
+				<td style="background-color: {GRID[j][i]}" onclick={guess(i, j)}>
 					<Selection guesses={getGuesses(i, j)} index={0} />
 				</td>
 	{/each}
@@ -268,7 +266,7 @@
 			<tbody>
 {#each gPlayers as i}
 				<tr>
-					<th style="background-color: #{i.colour}"></th>
+					<th style="background-color: {i.colour}"></th>
 					<td>{i.name}</td>
 					<td>{i.score}</td>
 				</tr>
@@ -299,7 +297,7 @@
 
 	h1
 	{
-		color: #ff3e00;
+		color: #FF3E00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
